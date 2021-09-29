11dc53e30178df6bc38e39982a96b4ac74a33c01
Gustavo Tiago <gu.tiagosilva@gmail.com>
2021-09-29 10:23:59 +1000

    adding config for login

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,17 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div className="container-login">
-      <img src="/logo.svg" alt="Logo Fiap"/>
+      <img src="/logo.svg" alt="Logo Fiap" className="logo"/>
       <form>
         <div className="input">
           <img src="/mail.svg" alt="Logo Fiap"/>
-          <input type="text" />
+          <input type="text" placeholder="Informe seu email"/>
         </div>
         <div className="input">
           <img src="/lock.svg" alt="Logo Fiap"/>
-          <input type="password" />
+          <input type="password" placeholder="Informe sua senha"/>
         </div>
-        <button type="button">Login</button>
+        <button type="button" className="button">Login</button>
       </form>
     </div>
   )
